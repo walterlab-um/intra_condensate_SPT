6871ccd248dab0528d0e51e73a8dea93c785f28b
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-05-18 02:49:53 -0400

    remove two pairs

--- plot_SPT_metrices/boxplot_percent_static_constrained_plus_stats.py
+++ plot_SPT_metrices/boxplot_percent_static_constrained_plus_stats.py
@@ -32,8 +32,6 @@ df_plot = df_save.melt(
 box_pairs = [
     ("0Dex, -, 0h", "0Dex, -, 3h"),
     ("10Dex, -, 0h", "10Dex, -, 3h"),
-    ("0Dex, -, 3h", "0Dex, He, 1h"),
-    ("10Dex, -, 3h", "10Dex, He, 1h"),
     ("0Dex, -, 0h", "0Dex, He, 1h"),
     ("10Dex, -, 0h", "10Dex, He, 1h"),
     ("0Dex, He, 1h", "0Dex, Ce, 1h"),
