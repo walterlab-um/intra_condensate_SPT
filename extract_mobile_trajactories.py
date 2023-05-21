317124de53b371b0e6147aa2ddfe7708be9696c2
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-05-21 02:36:16 -0400

    No need to separately extract constrained tracks based on alpha

--- extract_mobile_trajactories.py
+++ extract_mobile_trajactories.py
@@ -27,7 +27,7 @@ df_constrained = df_mobile[df_mobile["alpha"] <= 0.5]
 lst_fname_all = df_mobile["filename"].unique().tolist()
 # construct two new dataframe columns
 lst_df_segments_mobile = []
-lst_df_segments_constrained = []
+# lst_df_segments_constrained = []
 
 print("Total number of files:", str(len(lst_fname_all)))
 for fname in track(lst_fname_all):
@@ -41,24 +41,24 @@ for fname in track(lst_fname_all):
     ]
 
     # extract constrained tracks from current file
-    set_trackID_constrained = set(
-        df_constrained[df_constrained["filename"] == fname]["trackID"].unique()
-    )
-    df_current_file_constrained = df_current_file[
-        df_current_file["trackID"].isin(set_trackID_constrained)
-    ]
+    # set_trackID_constrained = set(
+    #     df_constrained[df_constrained["filename"] == fname]["trackID"].unique()
+    # )
+    # df_current_file_constrained = df_current_file[
+    #     df_current_file["trackID"].isin(set_trackID_constrained)
+    # ]
 
     # save
     lst_df_segments_mobile.append(df_current_file_mobile[["t", "x", "y"]])
-    lst_df_segments_constrained.append(df_current_file_constrained[["t", "x", "y"]])
+    # lst_df_segments_constrained.append(df_current_file_constrained[["t", "x", "y"]])
 
 # construct and save the two dataframe
 df_out_mobile = pd.concat(lst_df_segments_mobile, ignore_index=True)
 fname_save = "Mobile_tracks-" + basename(path_D_alpha_alltracks).split("alltracks_")[-1]
 df_out_mobile.to_csv(join(folder_save, fname_save), index=False)
 
-df_out_constrained = pd.concat(lst_df_segments_constrained, ignore_index=True)
-fname_save = (
-    "Constrained_tracks-" + basename(path_D_alpha_alltracks).split("alltracks_")[-1]
-)
-df_out_constrained.to_csv(join(folder_save, fname_save), index=False)
+# df_out_constrained = pd.concat(lst_df_segments_constrained, ignore_index=True)
+# fname_save = (
+#     "Constrained_tracks-" + basename(path_D_alpha_alltracks).split("alltracks_")[-1]
+# )
+# df_out_constrained.to_csv(join(folder_save, fname_save), index=False)
