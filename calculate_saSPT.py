c142bb9999a97da4797e46729f1e9626b6a61ae5
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-06-02 17:57:04 -0400

    rename, use the AIO file as input

--- calculate_saSPT.py
+++ calculate_saSPT.py
@@ -5,38 +5,26 @@ import pandas as pd
 from saspt import sample_detections, StateArray, RBME
 from tkinter import filedialog as fd
 from rich.progress import track
-import pickle
 
-print("Choose all track files to be pooled for saSPT:")
-lst_files = list(fd.askopenfilenames())
 
-lst_x = []
-lst_y = []
-lst_trackID = []
-lst_frame = []
-for file in track(lst_files, description="Pooling..."):
-    df = pd.read_csv(file)
-    lst_x.extend(df.x)
-    lst_y.extend(df.y)
-    if len(lst_trackID) == 0:
-        lst_trackID.extend(df.trackID)
-    else:
-        lst_trackID.extend(np.array(df.trackID + np.max(lst_trackID) + 1, dtype=int))
-    lst_frame.extend(df.t.astype(int))
-
-df_pooled = pd.DataFrame(
-    {"x": lst_x, "y": lst_y, "trajectory": lst_trackID, "frame": lst_frame,},
-    dtype=object,
-)
-# saSPT
-settings = dict(
+saSPT_settings = dict(
     likelihood_type=RBME,
     pixel_size_um=0.117,
     frame_interval=0.02,
     focal_depth=0.7,
     progress_bar=True,
 )
-SA = StateArray.from_detections(df_pooled, **settings)
+os.chdir("/Volumes/AnalysisGG/PROCESSED_DATA/RNA_SPT_in_FUS-May2023_wrapup")
+
+lst_fname = [f for f in os.listdir(".") if f.startswith("SPT_results_AIO")]
+
+for 
+df_pooled = pd.DataFrame(
+    {"x": lst_x, "y": lst_y, "trajectory": lst_trackID, "frame": lst_frame,},
+    dtype=object,
+)
+# saSPT
+SA = StateArray.from_detections(df_pooled, **saSPT_settings)
 posterior_occs = SA.posterior_occs
 occurance = np.sum(posterior_occs, axis=1)
 diff_coefs, loc_errors = SA.parameter_values
