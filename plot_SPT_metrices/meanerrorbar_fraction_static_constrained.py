fa80c48c6a3bf7868ea6d831abf7cf86533484f8
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-06-10 23:32:04 -0400

    change to per replicate instead of FOV

--- plot_SPT_metrices/meanerrorbar_fraction_static_constrained.py
+++ plot_SPT_metrices/meanerrorbar_fraction_static_constrained.py
@@ -4,27 +4,104 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statannot import add_stat_annotation
+from rich.progress import track
 
 sns.set(color_codes=True, style="white")
-
 pd.options.mode.chained_assignment = None  # default='warn'
 
-color_palette = [
-    "#001219",
-    "#005f73",
-    "#0a9396",
-    "#94d2bd",
-    "#e9d8a6",
-    "#ee9b00",
-    "#ca6702",
-    "#bb3e03",
-    "#ae2012",
-    "#9b2226",
-]
+
 os.chdir("/Volumes/AnalysisGG/PROCESSED_DATA/RNA_SPT_in_FUS-May2023_wrapup")
-# Run script "barplot_N_traj_total2mobile2constrained_per_FOV_per_condition.py" to get the following csv file
-df_save = pd.read_csv("N_and_Fraction_per_FOV.csv")
-df_plot = df_save.melt(
+
+# Displacement threshold for non static molecules
+threshold_disp = 0.2  # unit: um
+# alpha component threshold for constrained diffusion
+threshold_alpha = 0.5
+
+dict_input_path = {
+    "0Dex, -, 0h": "SPT_results_AIO-0Dex_noTotR_0h.csv",
+    "0Dex, -, 3h": "SPT_results_AIO-0Dex_noTotR_3h.csv",
+    "0Dex, He, 1h": "SPT_results_AIO-0Dex_Hela_1h.csv",
+    "0Dex, Ce, 1h": "SPT_results_AIO-0Dex_Cerebral_1h.csv",
+    "0Dex, Sp, 1h": "SPT_results_AIO-0Dex_Spinal_1h.csv",
+    "10Dex, -, 0h": "SPT_results_AIO-10Dex_noTotR_0h.csv",
+    "10Dex, -, 3h": "SPT_results_AIO-10Dex_noTotR_3h.csv",
+    "10Dex, He, 0h": "SPT_results_AIO-10Dex_Hela_0h.csv",
+    "10Dex, Ce, 0h": "SPT_results_AIO-10Dex_Cerebral_0h.csv",
+    "10Dex, Sp, 0h": "SPT_results_AIO-10Dex_Spinal_0h.csv",
+}
+# calculate error bounds
+um_per_pixel = 0.117
+s_per_frame = 0.02
+static_err = 0.016
+um_per_pxl = 0.117
+link_max = 3
+log10D_low = np.log10(static_err**2 / (4 * (s_per_frame)))
+log10D_high = np.log10((um_per_pxl * link_max) ** 2 / (4 * (s_per_frame)))
+
+# Output file structure
+columns = [
+    "label",
+    "replicate_prefix",
+    "N, Total",
+    "N, Mobile",
+    "N, Constrained",
+    "Static Fraction",
+    "Constrained Fraction",
+]
+# construct output dataframe
+lst_rows_of_df = []
+for key in track(dict_input_path.keys()):
+    df_current = pd.read_csv(dict_input_path[key])
+    df_current = df_current.astype(
+        {"linear_fit_log10D": float, "Displacement_um": float, "alpha": float}
+    )
+    # all filenames within the current condition/file
+    all_filenames = df_current["filename"].unique().tolist()
+    # filename prefix for each replicate
+    replicate_prefixs = np.unique([f.split("FOV")[0] for f in all_filenames])
+
+    for prefix in replicate_prefixs:
+        current_replicate_filenames = [f for f in all_filenames if prefix in f]
+        df_current_replicate = df_current[
+            df_current["filename"].isin(current_replicate_filenames)
+        ]
+
+        df_mobile_byD = df_current_replicate[
+            df_current_replicate["linear_fit_log10D"] > log10D_low
+        ]
+        df_mobile = df_mobile_byD[df_mobile_byD["Displacement_um"] >= threshold_disp]
+        df_constrained = df_mobile[df_mobile["alpha"] <= threshold_alpha]
+
+        N_total = df_current_replicate.shape[0]
+        N_mobile = df_mobile.shape[0]
+        N_constrained = df_constrained.shape[0]
+
+        if N_constrained < 1:
+            continue
+
+        F_static = (N_total - N_mobile) / N_total
+        F_constrained = N_constrained / N_mobile
+
+        # save
+        lst_rows_of_df.append(
+            [
+                key,
+                prefix,
+                N_total,
+                N_mobile,
+                N_constrained,
+                F_static,
+                F_constrained,
+            ]
+        )
+
+df_save = pd.DataFrame.from_records(
+    lst_rows_of_df,
+    columns=columns,
+)
+df_save.to_csv("N_and_Fraction_per_replicate.csv", index=False)
+
+df_plot = df_save[df_save["N, Total"] > 1000].melt(
     id_vars=["label"],
     value_vars=["Static Fraction", "Constrained Fraction"],
 )
@@ -41,7 +118,18 @@ box_pairs = [
     ("10Dex, Ce, 0h", "10Dex, Sp, 0h"),
 ]
 
-
+color_palette = [
+    "#001219",
+    "#005f73",
+    "#0a9396",
+    "#94d2bd",
+    "#e9d8a6",
+    "#ee9b00",
+    "#ca6702",
+    "#bb3e03",
+    "#ae2012",
+    "#9b2226",
+]
 plt.figure(figsize=(4, 6), dpi=300)
 data = df_plot[df_plot["variable"] == "Static Fraction"]
 ax = sns.pointplot(
