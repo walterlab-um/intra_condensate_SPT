5ed1d0034e869605dbc0714ddf0093dc5bac4eea
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-06-12 01:07:51 -0400

    plot all replicates in a condition + find peak

--- plot_SPT_metrices/lineplot_saspt.py
+++ plot_SPT_metrices/lineplot_saspt.py
@@ -2,13 +2,17 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy.signal import find_peaks
 import seaborn as sns
 from rich.progress import track
 
 sns.set(color_codes=True, style="white")
 
-os.chdir("/Volumes/AnalysisGG/PROCESSED_DATA/RNA_SPT_in_FUS-May2023_wrapup")
-probability_upper_limit = 0.03
+# plot all replicates in a single condition
+os.chdir(
+    "/Volumes/AnalysisGG/PROCESSED_DATA/RNA_SPT_in_FUS-May2023_wrapup/saSPT_per_replicate"
+)
+plot_xlim = [-2, 0]
 
 color_palette = [
     "#001219",
@@ -22,17 +26,17 @@ color_palette = [
     "#ae2012",
     "#9b2226",
 ]
-dict_input_path = {
-    "0Dex, -, 0h": "saSPT_output-0Dex_noTotR_0h.csv",
-    "0Dex, -, 3h": "saSPT_output-0Dex_noTotR_3h.csv",
-    "0Dex, He, 1h": "saSPT_output-0Dex_Hela_1h.csv",
-    "0Dex, Ce, 1h": "saSPT_output-0Dex_Cerebral_1h.csv",
-    "0Dex, Sp, 1h": "saSPT_output-0Dex_Spinal_1h.csv",
-    "10Dex, -, 0h": "saSPT_output-10Dex_noTotR_0h.csv",
-    "10Dex, -, 3h": "saSPT_output-10Dex_noTotR_3h.csv",
-    "10Dex, He, 0h": "saSPT_output-10Dex_Hela_0h.csv",
-    "10Dex, Ce, 0h": "saSPT_output-10Dex_Cerebral_0h.csv",
-    "10Dex, Sp, 0h": "saSPT_output-10Dex_Spinal_0h.csv",
+dict_condition_prefix = {
+    "0Dex, -, 0h": "0Dex_noTotR_0h",
+    "0Dex, -, 3h": "0Dex_noTotR_3h",
+    "0Dex, He, 1h": "0Dex_Hela_1h",
+    "0Dex, Ce, 1h": "0Dex_Cerebral_1h",
+    "0Dex, Sp, 1h": "0Dex_Spinal_1h",
+    "10Dex, -, 0h": "10Dex_noTotR_0h",
+    "10Dex, -, 3h": "10Dex_noTotR_3h",
+    "10Dex, He, 0h": "10Dex_Hela_0h",
+    "10Dex, Ce, 0h": "10Dex_Cerebral_0h",
+    "10Dex, Sp, 0h": "10Dex_Spinal_0h",
 }
 
 
@@ -50,26 +54,52 @@ def extract_log10D_density(df_current_file):
     return df_toplot
 
 
-lst_keys = list(dict_input_path.keys())
-i = 0
+lst_keys = list(dict_condition_prefix.keys())
 
-plt.figure(figsize=(5, 4), dpi=300)
-for i in track(range(len(lst_keys))):
-    key = lst_keys[i]
-    color = color_palette[i]
-    df_current_file = pd.read_csv(dict_input_path[key], dtype=float)
+for key in track(lst_keys):
+    # list all replicates in the current condition
+    lst_fname_current_condition = [
+        f
+        for f in os.listdir(".")
+        if f.endswith(".csv") and f.startswith(dict_condition_prefix[key])
+    ]
 
-    df_toplot = extract_log10D_density(df_current_file)
+    plt.figure(figsize=(5, 4), dpi=300)
+    i = 0
+    for fname in lst_fname_current_condition:
+        df_current_file = pd.read_csv(fname, dtype=float)
+        df_toplot = extract_log10D_density(df_current_file)
+        df_toplot = df_toplot[df_toplot["log10D"] > plot_xlim[0]]
 
-    sns.lineplot(
-        data=df_toplot, x="log10D", y="Probability", color=color_palette[i], label=key
+        sns.lineplot(
+            data=df_toplot,
+            x="log10D",
+            y="Probability",
+            color=sns.color_palette()[i],
+            label=fname,
+        )
+        # find peaks
+        log10D = df_toplot["log10D"].to_numpy(dtype=float)
+        proportion = df_toplot["Probability"].to_numpy(dtype=float)
+        peaks_idx, _ = find_peaks(proportion)
+        for x in log10D[peaks_idx]:
+            plt.plot(
+                log10D[peaks_idx],
+                proportion[peaks_idx],
+                "*",
+                color=sns.color_palette()[i],
+                markersize=10,
+            )
+            plt.axvline(x, color=sns.color_palette()[i], ls="--", lw=1, alpha=0.3)
+        i += 1
+
+    # plt.xlim(df_toplot["log10D"].iloc[0], df_toplot["log10D"].iloc[-1])
+    plt.xlim(plot_xlim[0], plot_xlim[1])
+    plt.xlabel(r"log$_{10}$D, $\mu$m$^2$/s", weight="bold")
+    plt.ylabel("Proportion", weight="bold")
+    plt.legend(fontsize=5)
+    plt.tight_layout()
+    plt.savefig(
+        "saSPT_per_replicate-" + dict_condition_prefix[key] + ".png", format="png"
     )
-
-plt.xlim(df_toplot["log10D"].iloc[0], df_toplot["log10D"].iloc[-1])
-plt.ylim(0, probability_upper_limit)
-plt.xlabel(r"log$_{10}$D, $\mu$m$^2$/s", weight="bold")
-plt.ylabel("Probability", weight="bold")
-plt.legend(ncol=2, fontsize=11)
-plt.tight_layout()
-plt.savefig("saSPT_all.png", format="png")
-plt.close()
+    plt.close()
