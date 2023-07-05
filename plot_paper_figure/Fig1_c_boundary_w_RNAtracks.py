06ce3907c62fe16847354b7ea1546130d0020889
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-07-05 02:32:34 -0400

    zoom in version

--- plot_paper_figure/Fig1_c_boundary_w_RNAtracks.py
+++ plot_paper_figure/Fig1_c_boundary_w_RNAtracks.py
@@ -7,6 +7,9 @@ from rich.progress import track
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
+zoom_in_x = (210, 310)
+zoom_in_y = (290, 390)
+
 um_per_pixel = 0.117
 folder_save = "/Volumes/lsa-nwalter/Guoming_Gao_turbo/Walterlab_server/PROCESSED_DATA/RNA-diffusion-in-FUS/RNAinFUS_PaperFigures/Fig1_system design/c_boundary_w_RNAtracks/"
 os.chdir(folder_save)
@@ -17,9 +20,8 @@ cmap_RNA = clr.LinearSegmentedColormap.from_list(
     "black2red", ["#333232", "#9a3324"], N=200
 )
 
-
 # scale bar
-scalebar_length_um = 5
+scalebar_length_um = 1
 um_per_pixel = 0.117
 scalebar_length_pxl = scalebar_length_um / um_per_pixel
 
@@ -49,7 +51,7 @@ def list_like_string_to_xyt(list_like_string):
     return np.array(lst_xyt, dtype=float)
 
 
-plt.figure(dpi=1000)
+plt.figure(dpi=300, figsize=(5, 5))
 ## plot condensate boundaries
 for condensateID in track(
     df_condensate_current_FOV["condensateID"].unique(), description="Plot condensates"
@@ -63,13 +65,13 @@ for condensateID in track(
         xy = str_condensate_xy.split(", ")
         x.append(int(xy[0]))
         y.append(int(xy[1]))
-    plt.plot(x, y, "-", color=line_color, linewidth=0.5)
+    plt.plot(x, y, "-", color=line_color, linewidth=2, alpha=0.7)
     # still the last closing line will be missing, get it below
     xlast = [x[-1], x[0]]
     ylast = [y[-1], y[0]]
-    plt.plot(xlast, ylast, "-", color=line_color, linewidth=0.5)
-plt.xlim(0, 418)
-plt.ylim(0, 674)
+    plt.plot(xlast, ylast, "-", color=line_color, linewidth=2, alpha=0.7)
+# plt.xlim(0, 418)
+# plt.ylim(0, 674)
 
 ## plot RNA tracks
 for _, row in track(df_RNA_current_FOV.iterrows(), description="Plot RNAs"):
@@ -83,12 +85,27 @@ for _, row in track(df_RNA_current_FOV.iterrows(), description="Plot RNAs"):
             y[i : i + 2],
             "-",
             color=cmap_RNA(np.mean(t[i : i + 2]) / 200),
-            linewidth=0.1,
+            linewidth=0.5,
         )
 
-## plot scale bar
-plt.plot([10, 10 + scalebar_length_pxl], [10, 10], "-", color=scalebar_color, lw=5)
+plt.tight_layout()
 plt.axis("scaled")
 plt.axis("off")
-plt.savefig("Fig1_c_boundary_w_RNAtracks.png", format="png", bbox_inches="tight")
+## plot scale bar
+
+# non-zoom in
+# plt.plot([10, 10 + scalebar_length_pxl], [10, 10], "-", color=scalebar_color, lw=5)
+
+# zoom in
+plt.xlim(zoom_in_x[0], zoom_in_x[1])
+plt.ylim(zoom_in_y[0], zoom_in_y[1])
+plt.plot(
+    [zoom_in_x[0] + 5, zoom_in_x[0] + 5 + scalebar_length_pxl],
+    [zoom_in_y[0] + 5, zoom_in_y[0] + 5],
+    "-",
+    color=scalebar_color,
+    lw=5,
+)
+# plt.show()
+plt.savefig("Fig1_c_boundary_w_RNAtracks_zoomin.png", format="png", bbox_inches="tight")
 plt.close()
