8f2a605c1b9105a57282f8bdb7fbb86fb829f2c2
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-07-04 20:23:54 -0400

    Create Fig1_b_condensate_FOV_w_inlet.py

---
+++ plot_paper_figure/Fig1_b_condensate_FOV_w_inlet.py
@@ -0,0 +1,59 @@
+from tifffile import imread
+import os
+from os.path import join, dirname, basename
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tkinter import filedialog as fd
+from rich.progress import track
+
+pd.options.mode.chained_assignment = None  # default='warn'
+
+um_per_pixel = 0.117
+fpath = "/Volumes/lsa-nwalter/Guoming_Gao_turbo/Walterlab_server/PROCESSED_DATA/RNA-diffusion-in-FUS/RNAinFUS_PaperFigures/Fig1_system design/b_condensate_FOV_w_inlet/20221031-FL_noTR_noDex_20ms_Replicate1_FOV-8-condensates_AveProj_Simple Segmentation.tif"
+folder_save = dirname(fpath)
+os.chdir(folder_save)
+
+plow = 0.5  # imshow intensity percentile
+phigh = 85
+linecolor = "#00274C"
+
+# scale bar
+scalebar_length_um = 5
+um_per_pixel = 0.117
+scalebar_length_pxl = scalebar_length_um / um_per_pixel
+
+ilastik_output = imread(fpath)
+img = imread(fpath[:-24] + ".tif")
+
+# Cropping
+ilastik_output = ilastik_output[35:235, 150:350]
+img = img[35:235, 150:350]
+
+mask_all_condensates = 2 - ilastik_output  # background label=2, condensate label=1
+# find contours coordinates in binary edge image. contours here is a list of np.arrays containing all coordinates of each individual edge/contour.
+contours, _ = cv2.findContours(
+    mask_all_condensates, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE
+)
+
+
+plt.figure()
+# Contrast stretching
+vmin, vmax = np.percentile(img, (plow, phigh))
+plt.imshow(img, cmap="Blues", vmin=vmin, vmax=vmax)
+for cnt in contours:
+    x = cnt[:, 0][:, 0]
+    y = cnt[:, 0][:, 1]
+    plt.plot(x, y, "-", color=linecolor, linewidth=2)
+    # still the last closing line will be missing, get it below
+    xlast = [x[-1], x[0]]
+    ylast = [y[-1], y[0]]
+    plt.plot(xlast, ylast, "-", color=linecolor, linewidth=2)
+plt.xlim(0, img.shape[0])
+plt.ylim(0, img.shape[1])
+plt.plot([10, 10 + scalebar_length_pxl], [10, 10], "-", color="black", lw=7)
+plt.axis("scaled")
+plt.axis("off")
+plt.savefig("Fig1_b_condensate_FOV.png", format="png", bbox_inches="tight", dpi=600)
+plt.close()
