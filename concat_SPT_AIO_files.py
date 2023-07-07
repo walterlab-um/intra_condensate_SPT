5030359564d81a07c63183bf9f8a975034bd344c
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-07-07 11:09:52 -0400

    Add a column of filename

--- concat_SPT_AIO_files.py
+++ concat_SPT_AIO_files.py
@@ -1,5 +1,6 @@
 import os
-from os.path import dirname
+from os.path import dirname, basename
+import numpy as np
 import pandas as pd
 from rich.progress import track
 from tkinter import filedialog as fd
@@ -10,6 +11,8 @@ folder_data = dirname(lst_path_data[0])
 os.chdir(folder_data)
 lst_df = []
 for f in track(lst_path_data):
-    lst_df.append(pd.read_csv(f))
+    df = pd.read_csv(f)
+    df.insert(0, "filename", np.repeat(basename(f), df.shape[0]))
+    lst_df.append(df)
 df_all = pd.concat(lst_df)
 df_all.to_csv("SPT_results_AIO_concat-pleaserename.csv", index=False)
