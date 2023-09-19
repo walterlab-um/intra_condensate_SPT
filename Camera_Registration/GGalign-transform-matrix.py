f72b5fe7f3e060ee80fe8cde39ab9cee00abd1db
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-09-19 16:57:00 -0400

    transformation matrix on both left and right

--- Camera_Registration/GGalign-transform-matrix.py
+++ Camera_Registration/GGalign-transform-matrix.py
@@ -28,7 +28,8 @@ sz = img_left.shape
 warp_mode = cv2.MOTION_HOMOGRAPHY
 
 # Define a 3x3 matrices and initialize the matrix to identity
-warp_matrix = np.eye(3, 3, dtype=np.float32)
+warp_matrix_right = np.eye(3, 3, dtype=np.float32)
+warp_matrix_left = np.eye(3, 3, dtype=np.float32)
 
 # Specify the number of iterations.
 number_of_iterations = 100
@@ -45,20 +46,37 @@ criteria = (
 )
 
 # Run the ECC algorithm. The results are stored in warp_matrix.
-(cc, warp_matrix) = cv2.findTransformECC(
-    img_left, img_right, warp_matrix, warp_mode, criteria
+(cc, warp_matrix_right) = cv2.findTransformECC(
+    img_left, img_right, warp_matrix_right, warp_mode, criteria
+)
+(cc, warp_matrix_left) = cv2.findTransformECC(
+    img_right, img_left, warp_matrix_left, warp_mode, criteria
 )
 
 # Save the matrix
 folderpath = os.path.dirname(path)
-pickle.dump(warp_matrix, open(os.path.join(folderpath, "ONI_warp_matrix.p"), "wb"))
+pickle.dump(
+    warp_matrix_right, open(os.path.join(folderpath, "ONI_warp_matrix_right.p"), "wb")
+)
+pickle.dump(
+    warp_matrix_left, open(os.path.join(folderpath, "ONI_warp_matrix_left.p"), "wb")
+)
 
 # Test and save alignment results on beads
 img_right_unit16 = cv2.imread(path[:-4] + "-right.tif", -1)
 img_right_aligned = cv2.warpPerspective(
     img_right_unit16,
-    warp_matrix,
+    warp_matrix_right,
     (sz[1], sz[0]),
     flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP,
 )
 imwrite(path[:-4] + "-right-aligned.tif", img_right_aligned, imagej=True)
+
+img_left_unit16 = cv2.imread(path[:-4] + "-left.tif", -1)
+img_left_aligned = cv2.warpPerspective(
+    img_left_unit16,
+    warp_matrix_left,
+    (sz[1], sz[0]),
+    flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP,
+)
+imwrite(path[:-4] + "-left-aligned.tif", img_left_aligned, imagej=True)
