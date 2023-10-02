da99503ed1aa788e2daa2f0dc0c305210df9be8d
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-10-02 17:00:05 -0400

    convert format for HMM-Bayes

---
+++ tracks2mat_for_HMM.py
@@ -0,0 +1,44 @@
+from scipy.io import savemat
+import pandas as pd
+import numpy as np
+from tkinter import filedialog as fd
+
+
+fs = 0.02
+umperpx = 0.117
+locerror = 0.025
+dict_cfg = {
+    "fs": fs,
+    "umperpx": umperpx,
+    "locerror": locerror,
+}
+
+print("Choose one reformatted track file")
+path = fd.askopenfilename()
+df_all = pd.read_csv(path)
+
+df_all = df_all.astype(
+    {
+        "trackID": int,
+        "x": float,
+        "y": float,
+        "t": float,
+    }
+)
+lst_trackID = df_all["trackID"].unique()
+
+for trackID in lst_trackID:
+    df_track = df_all[df_all["trackID"] == trackID].sort_values("t")
+    x = df_track["x"].to_numpy(float)
+    y = df_track["y"].to_numpy(float)
+
+    track = np.vstack([x, y])
+
+    fpath_out = path[:-4] + ".mat"
+
+    mdict = {
+        "cfg": dict_cfg,
+        "track": track,
+    }
+
+    savemat(fpath_out, mdict)
