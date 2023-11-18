0fe94a0a6f78b1fe3dbfa3b8f5428a50c0c5f824
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-11-17 19:00:40 -0500

    typo fix

--- plot_paper_figure/random_sampling_forQDamine.py
+++ plot_paper_figure/random_sampling_forQDamine.py
@@ -12,7 +12,7 @@ df = pd.read_csv(fpath)
 os.chdir(dirname(fpath))
 
 for i in track(range(5)):
-    indexes_picked = sample(range(df.shape[0], k=round(df.shape[0] / 10)))
+    indexes_picked = sample(range(df.shape[0]), k=round(df.shape[0] / 10))
     df_out = df.iloc[indexes_picked]
     df_out.to_csv(basename(fpath)[:-4] + "-sample-" + str(i) + ".csv", index=False)
     i += 1
