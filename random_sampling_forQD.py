9fcc95b6aa39fdf826d44f6b0c387410c7170d1e
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-12-13 21:04:33 -0500

    move

--- random_sampling_forQD.py
+++ random_sampling_forQD.py
@@ -5,13 +5,14 @@ import pandas as pd
 from random import sample
 from rich.progress import track
 
+# Some QD datasets are too big for processing. This script random samples a fraction of the dataset
 
 fpath = fd.askopenfilename()
 
 df = pd.read_csv(fpath)
 os.chdir(dirname(fpath))
 
-for i in track(range(5)):
+for i in track(range(3)):
     indexes_picked = sample(range(df.shape[0]), k=round(df.shape[0] / 10))
     df_out = df.iloc[indexes_picked]
     df_out.to_csv(basename(fpath)[:-4] + "-sample-" + str(i) + ".csv", index=False)
