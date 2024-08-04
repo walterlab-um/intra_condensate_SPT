ab7e761dc43c36ca77d54e853233ef823207b310
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2024-08-04 00:37:00 -0400

    Create concat_SPT_AIO_files.py

---
+++ concat_SPT_AIO_files.py
@@ -0,0 +1,20 @@
+import os
+from os.path import dirname, basename
+import numpy as np
+import pandas as pd
+from rich.progress import track
+from tkinter import filedialog as fd
+
+print("Choose all SPT_results_AIO_xxxxx.csv files within the same condition:")
+lst_path_data = list(
+    fd.askopenfilenames(filetypes=(("CSV files", "*.csv"), ("All files", "*.*")))
+)
+folder_data = dirname(lst_path_data[0])
+os.chdir(folder_data)
+lst_df = []
+for f in track(lst_path_data):
+    df = pd.read_csv(f)
+    df.insert(0, "filename", np.repeat(basename(f), df.shape[0]))
+    lst_df.append(df)
+df_all = pd.concat(lst_df, ignore_index=True)
+df_all.to_csv("SPT_results_AIO_concat-pleaserename.csv", index=False)
