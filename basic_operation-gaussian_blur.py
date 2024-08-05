6a0c9454422d913065a48c9d26903d3f3bda8f1a
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2024-08-05 19:56:03 -0400

    Update basic_operation-gaussian_blur.py

--- basic_operation-gaussian_blur.py
+++ basic_operation-gaussian_blur.py
@@ -1,7 +1,5 @@
 from tifffile import imread, imwrite
 from scipy.ndimage import gaussian_filter
-from skimage.filters import difference_of_gaussians
-from skimage.util import img_as_uint
 from tkinter import filedialog as fd
 import numpy as np
 from rich.progress import track
@@ -21,6 +19,6 @@ for f in track(lst_files):
     video_out = np.stack(video_out)
     fsave = f[:-4] + "-smoothed.tif"
     if len(video.shape) < 3:  # image:
-        imwrite(fsave, img_as_uint(video_out), imagej=True)
+        imwrite(fsave, video_out)
     elif len(video.shape) > 2:  # video:
-        imwrite(fsave, img_as_uint(video_out), imagej=True, metadata={"axes": "TYX"})
+        imwrite(fsave, video_out, metadata={"axes": "TYX"})
